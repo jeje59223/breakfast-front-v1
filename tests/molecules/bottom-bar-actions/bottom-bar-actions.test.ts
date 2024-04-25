bd1715b71fcdb3a4b15b822c1e29ed6e0b7235a9
Jerome <jerome.cnockaert@leroymerlin.fr>
2024-04-25 14:16:43 +0200

    feat(BREAKFAST): add tests on bottom bar actions component

---
+++ tests/molecules/bottom-bar-actions/bottom-bar-actions.test.ts
@@ -0,0 +1,41 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { mount, VueWrapper } from '@vue/test-utils';
+import { createTestingPinia } from '@pinia/testing';
+import BottomBarActions from '../../../src/components/molecules/bottom-bar-actions/BottomBarActions.vue';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+describe('BottomBarActions', () => {
+    let wrapper: VueWrapper
+    let vm: any
+    beforeEach(() => {
+        wrapper = mount(BottomBarActions, {
+            global: {
+                plugins: [vuetify, createTestingPinia()],
+            },
+            props: {
+                text: 'Save'
+            }
+        }) as VueWrapper
+
+        vm = wrapper.vm as any
+    })
+
+    it('should render BottomBarActions correctly', async () => {
+        const bottomBar = wrapper.find('[tnr-id="Bottom-bar-actions"]')
+        const bottonBarButton = wrapper.find('[tnr-id="Bottom-bar-actions__save"]')
+
+        await bottonBarButton.trigger('click')
+
+        expect(bottomBar.exists()).toBeTruthy()
+        expect(bottonBarButton.exists()).toBeTruthy()
+        expect(bottonBarButton.text()).toBe('Save')
+        expect(wrapper.emitted('save')).toBeTruthy()
+    });
+})
