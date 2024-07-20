a3ce8bf5a612481491f746790c92e3bc22f57c7f
Jerome <jerome.cnockaert@leroymerlin.fr>
2024-07-20 13:18:31 +0200

    feat(BREAKFAST): add tests

--- vite.config.ts
+++ vite.config.ts
@@ -26,5 +26,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8086',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
